877f877e19b4962040f1644fc73362c680dffc91
Zach Carrera <zachcarrera@gmail.com>
2023-03-18 20:04:17 -0700

    add Advertisement component

---
+++ src/components/Advertisement.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Advertisement extends Component {
+    render() {
+        return <div className="advertisement"></div>;
+    }
+}
+
+export default Advertisement;
