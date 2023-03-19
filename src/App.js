bd720d7f3422a24bc1292466b1596b13ec304f8f
Zach Carrera <zachcarrera@gmail.com>
2023-03-18 20:00:03 -0700

    set heirarchy of components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Main from "./components/Main";
+import SubContents from "./components/SubContents";
+import Advertisement from "./components/Advertisement";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Header />
+            <div className="row">
+                <Navigation />
+                <Main>
+                    <div className="row">
+                        <SubContents />
+                        <SubContents />
+                        <SubContents />
+                    </div>
+                    <Advertisement />
+                </Main>
+            </div>
+        </div>
+    );
 }
 
 export default App;
