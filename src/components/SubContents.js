f8b8daaa0fd6062caf4219cf1c693cb9718f4b7f
Zach Carrera <zachcarrera@gmail.com>
2023-03-18 20:07:07 -0700

    add SubConents component

---
+++ src/components/SubContents.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class SubContents extends Component {
+    render() {
+        return <div className="sub-content"></div>;
+    }
+}
+
+export default SubContents;
